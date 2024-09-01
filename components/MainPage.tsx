1003d3aa95f75e98ffd4d22f4f64a207aa502e71
Shubhraj Prasad Singh <ashu.shubhraj@gmail.com>
2024-09-01 12:43:12 +0530

    Cart logic added

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -6,23 +6,13 @@ import CartSidebar from './CartSidebar/CartSidebar';
 import Checkout from './Checkout/Checkout';
 import { Item } from '@/utils/types';
 import { items } from '@/assets/assets';
+import { useCart } from './Context/CartContext';
 
 const MainPage = () => {
-  const [cartItems, setCartItems] = useState([]);
   const [showCart, setShowCart] = useState(false);
   const [discountCode, setDiscountCode] = useState('');
 
-  const handleAddToCart = (itemId: string) => {
-    // Logic to add item to cart
-  };
-
-  const handleRemoveFromCart = (itemId: string) => {
-    // Logic to remove item from cart
-  };
-
-  const handleUpdateQuantity = (itemId: string, quantity: number) => {
-    // Logic to update item quantity in cart
-  };
+  const { cartItems, addToCart, removeFromCart, updateQuantity } = useCart();
 
   const handleCheckout = () => {
     // Logic to handle checkout
@@ -33,14 +23,12 @@ const MainPage = () => {
       <Header onViewCart={() => setShowCart(true)} />
       <div className="grid grid-cols-6 gap-4 justify-items-center my-8 space-y-4">
         {items.map(item => (
-          <ItemCard key={item.id} item={item} onAddToCart={handleAddToCart} onRemoveFromCart={handleRemoveFromCart} />
+          <ItemCard key={item.id} item={item} onAddToCart={()=> addToCart(item)} onRemoveFromCart={()=>removeFromCart(item.id)} />
         ))}
       </div>
       {showCart && (
         <CartSidebar
-          cartItems={cartItems}
-          onUpdateQuantity={handleUpdateQuantity}
-          onCheckout={handleCheckout}
+          onClose={()=> setShowCart(false)}
         />
       )}
       {/* Checkout component can be rendered conditionally based on the checkout flow */}
